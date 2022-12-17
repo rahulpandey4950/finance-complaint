165509fd216f97d22a67a305c6f22f976de23526
Rahul Pandey <rahulpandey4950@gmail.com>
2022-12-17 17:57:08 +0530

    Prediction added to pipeline

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import argparse
 from dotenv import load_dotenv
 load_dotenv()
 from finance_complaint.exception import FinanceException
-from finance_complaint.pipeline import TrainingPipeline
+from finance_complaint.pipeline import TrainingPipeline, PredictionPipeline
 from finance_complaint.logger import logger
 from finance_complaint.config.pipeline.training import FinanceConfig
 import sys
@@ -18,10 +18,21 @@ def start_training(start=False):
     except Exception as e:
         raise FinanceException(e, sys)
 
+def start_prediction(start=False):
+    try:
+        if not start:
+            return None
+        print("Prediction started")
+        PredictionPipeline().start_batch_prediction()
+        
+    except Exception as e:
+        raise FinanceException(e, sys)
 
-def main(training_status):
+
+def main(training_status, prediction_status):
     try:
         start_training(start=training_status)
+        start_prediction(start=prediction_status)
     except Exception as e:
         raise FinanceException(e, sys)
 
@@ -30,10 +41,11 @@ if __name__ == "__main__":
     try:
         parser = argparse.ArgumentParser()
         parser.add_argument("--t", default=0, type=int, help="If provided true training will be done else not")
+        parser.add_argument("--p", default=0, type=int, help="If provided prediction will be done else not")
 
         args = parser.parse_args()
 
-        main(training_status=args.t)
+        main(training_status=args.t, prediction_status=args.p)
     except Exception as e:
         print(e)
         pass
